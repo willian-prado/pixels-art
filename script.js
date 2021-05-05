a500f06f7d34c0560b63f4cf4594afabfb29ee1f
Willian Prado <willian_prado@hotmail.com>
2021-05-05 18:03:01 -0300

    Resolve os requisitos 4 e 5

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //  Adiciona cada cor na paleta de cores
 function paletaCores() {
     let color = document.querySelectorAll(".color");
-    color[0].style.backgroundColor = "black";
+    color[0].style.backgroundColor = 'rgb(0, 0, 0)';
     for (let index = 1; index < color.length; index += 1) {
         color[index].style.backgroundColor = geraRGB();
     }
@@ -10,13 +10,33 @@ function paletaCores() {
 paletaCores();
 
 //  Gera uma cor aleatória
-function geraRGB(){
+function geraRGB() {
     const red = Math.ceil(Math.random() * 255);
     const green = Math.ceil(Math.random() * 255);
     const blue = Math.ceil(Math.random() * 255);
     const color = `rgb(${red}, ${green}, ${blue})`;
 
-    if (color !== 'rgb(255,255,255)'){
-        return color;
+    if (color !== 'rgb(255,255,255)') {
+        return color; 
     }
 }
+
+//  Adiciona o quadro de pixels
+let linhas = document.querySelectorAll('.linha');
+
+function createPixelBoard(linhas){
+    //let pixel = document.createElement('div');
+    for (let i = 0; i < linhas.length; i += 1){
+        console.log(linhas[i]);
+
+        for (let j = 0; j < 5; j += 1){
+            let pixel = document.createElement("div");
+            pixel.className = "pixel";
+            linhas[i].appendChild(pixel);
+        }
+    }
+}
+
+createPixelBoard(linhas);
+
+//  Define a cor preta como pré-selecionada 
