a780adee8e19248b4ce1c0d9be3823f67b5d5d03
Willian Prado <willian_prado@hotmail.com>
2021-05-05 15:38:52 -0300

    Resolve o requisito 3

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //  Adiciona cada cor na paleta de cores
-function paletaCores(){
+function paletaCores() {
     let color = document.querySelectorAll(".color");
-
-    for (let index = 0; index < color.length; index += 1){
+    color[0].style.backgroundColor = "black";
+    for (let index = 1; index < color.length; index += 1) {
         color[index].style.backgroundColor = geraRGB();
     }
 }
@@ -11,10 +11,10 @@ paletaCores();
 
 //  Gera uma cor aleatória
 function geraRGB(){
-    let red = Math.ceil(Math.random() * 255);
-    let green = Math.ceil(Math.random() * 255);
-    let blue = Math.ceil(Math.random() * 255);
-    let color = `rgb(${red}, ${green}, ${blue})`;
+    const red = Math.ceil(Math.random() * 255);
+    const green = Math.ceil(Math.random() * 255);
+    const blue = Math.ceil(Math.random() * 255);
+    const color = `rgb(${red}, ${green}, ${blue})`;
 
     if (color !== 'rgb(255,255,255)'){
         return color;
