4f292865d94f90d9efd582210333ec3f0ca422ec
Willian Prado <willian_prado@hotmail.com>
2021-05-06 01:11:52 -0300

    Resolve o requisito 9

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function geraRGB() {
     const blue = Math.ceil(Math.random() * 255);
     const color = `rgb(${red}, ${green}, ${blue})`;
 
-    if (color !== 'rgb(255,255,255)') {
+    if (color !== 'rgb(255, 255, 255)') {
         return color; 
     }
 }
@@ -29,14 +29,13 @@ function geraRGB() {
 //  Adiciona o quadro de pixels
 let linhas = document.querySelectorAll('.linha');
 
-function createPixelBoard(lines){
-    //let pixel = document.createElement('div');
-    for (let i = 0; i < linhas.length; i += 1){
+function createPixelBoard(lines) {
+    for (let i = 0; i < linhas.length; i += 1) {
         console.log(linhas[i]);
 
-        for (let j = 0; j < 5; j += 1){
-            let pixel = document.createElement("div");
-            pixel.className = "pixel";
+        for (let j = 0; j < 5; j += 1) {
+            const pixel = document.createElement('div');
+            pixel.className = 'pixel';
             linhas[i].appendChild(pixel);
         }
     }
@@ -45,34 +44,57 @@ function createPixelBoard(lines){
 createPixelBoard(linhas);
 
 //  Define a cor preta como pré-selecionada 
-
-function startColor(){
+function startColor() {
     let black = document.querySelector(".color");
     black.classList.add("selected");
 }
 
 startColor();
 
-console.log(document.querySelector(".color"));
+console.log(document.querySelector('.color'));
 
 // Reatribui a classe selected à cor selecionada na paleta
-
-function addSelected(event){
+function addSelected(event) {
     let selectedElement = document.querySelector(".selected");
     selectedElement.classList.remove('selected');
     event.target.classList.add("selected");
 }
 
 // Clicar em um pixel no quadro de Pixels transforma a sua cor para a cor selecionada na paleta
+function setColor() {
+    const boxes = document.querySelectorAll('.pixel');
 
-function setColor(){
-    const boxes = document.querySelectorAll(".pixel");
-
-    for (let index = 0; index < boxes.length; index += 1){
+    for (let index = 0; index < boxes.length; index += 1) {
         boxes[index].addEventListener('click', (event) => {
             event.target.style.backgroundColor = document.querySelector(".selected").style.backgroundColor;
-        })
+        });
     }
 }
 
 setColor();
+
+// Cria um botão que remove a cor de todos os pixels
+function createButton() {
+    const btnContainer = document.getElementById('button-container');
+    const btnClear = document.createElement('button');
+    btnClear.innerText = 'Limpar';
+    btnClear.id = 'clear-board';
+    btnContainer.appendChild(btnClear);
+}
+
+createButton();
+
+function clearBoard() {
+    const boxes = document.querySelectorAll('.pixel');
+
+    for (let index = 0; index < boxes.length; index += 1) {
+        boxes[index].style.backgroundColor = 'rgb(255, 255, 255)';
+    }
+}
+
+const buttonClear = document.getElementById('clear-board');
+buttonClear.addEventListener('click', clearBoard);
+
+//  Faça o quadro de pixels ser definido pela pessoa usuaria 
+
+
